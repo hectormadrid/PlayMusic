056d8bc776de967a0ac2aa5640e6e19a94cd3a94
hectormadrid <hectorfmf2002@gmail.com>
2021-11-09 18:34:55 -0300

    a

--- app/src/main/java/com/example/playmusic/MainActivity.java
+++ app/src/main/java/com/example/playmusic/MainActivity.java
@@ -1,14 +1,19 @@
-package com.example.playmusic;
+ package com.example.playmusic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+ public class MainActivity extends AppCompatActivity {
+    Button play_pause;
+    MediaPlayer mp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
